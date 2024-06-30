f5defa6385098e18db05a40b3399448c1480bcfc
sota-f625 <s7716xf625@gmail.com>
2024-06-30 21:17:09 +0900

    コンフリクト解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-<<<<<<< HEAD
   validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :nickname, presence: true
   validates :last_name, presence: true
@@ -12,10 +11,6 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u30fc]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u30fc]+\z/ }
   validates :birth_day, presence: true
-=======
 
 
-
-         
->>>>>>> 97b29856c01d6472aa30d8aded29607ff208ecd7
 end
