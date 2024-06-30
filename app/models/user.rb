d95d763336326f2f04bd8abef0153d416ed67d5a
sota-f625 <s7716xf625@gmail.com>
2024-06-30 15:10:56 +0900

    user_validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-
-
-         
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
 end
