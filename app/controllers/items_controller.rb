5e53f8516c27c47fa479b7f9103d5d99996e1c64
sota-f625 <s7716xf625@gmail.com>
2024-07-02 15:30:36 +0900

    ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,12 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
