ba74c2e597e74bf5f58a28f32de292b9c7775798
sota-f625 <s7716xf625@gmail.com>
2024-07-03 18:16:58 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_day
+  has_one :purchase
 
   validates :image, presence: true
   validates :name, presence: true
