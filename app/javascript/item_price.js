0e3d7a76f62d020011c5104a187efba604238497
sota-f625 <s7716xf625@gmail.com>
2024-07-03 15:28:09 +0900

    コードレビュー後修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,11 +1,8 @@
 const price = () => {
-  console.log("Event run"); // デバッグ用ログ
   const priceInput = document.getElementById("item-price");
   if (priceInput) {
-    console.log("Price input found."); // デバッグ用ログ
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
-      console.log("Input value:", inputValue); // デバッグ用ログ
 
       const addTaxDom = Math.floor(inputValue * 0.1);
       const addTaxDomElement = document.getElementById("add-tax-price");
@@ -16,10 +13,9 @@ const price = () => {
       salesProfitElement.textContent = salesProfit;
     });
   } else {
-    console.log("Price input not found."); // デバッグ用ログ
   }
 };
 
 document.addEventListener("DOMContentLoaded", price);
+document.addEventListener("turbo:load", price);
 document.addEventListener("turbo:render", price);
-console.log("Price activated"); // デバッグ用ログ
