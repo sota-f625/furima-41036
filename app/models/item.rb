08ad4dc9723daefa02707d00d57e97e3e7c317d8
sota-f625 <s7716xf625@gmail.com>
2024-07-03 18:22:24 +0900

    sold_out定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,4 +19,8 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  def sold_out
+    purchase.present?
+  end
 end
