a09a1e1e33c248f9908ae7fc66b1045a0f9b4fee
sota-f625 <s7716xf625@gmail.com>
2024-07-03 23:42:34 +0900

    ビューの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,7 +20,7 @@ class Item < ApplicationRecord
   validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-  def sold_out
-    purchase.present?
-  end
+  # def sold_out
+  #   purchase.present?
+  # end
 end
