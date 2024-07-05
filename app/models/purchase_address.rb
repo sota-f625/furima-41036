8dca834dcf422461a20666102011b1df93d434bb
sota-f625 <s7716xf625@gmail.com>
2024-07-05 17:27:34 +0900

    モデルの修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,7 +6,7 @@ class PurchaseAddress
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
-    validates :addresses
+    validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
@@ -15,6 +15,6 @@ class PurchaseAddress
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, purchase_id: purchase.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
