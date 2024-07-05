6ee7f34f7a49e1a907ac888f08dd5cab83c1c389
sota-f625 <s7716xf625@gmail.com>
2024-07-05 15:26:18 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   
   root "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :purchases, only: [:index, :create]
 
 end
