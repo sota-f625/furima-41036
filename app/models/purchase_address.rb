a6c8758ae48d2eb1655205ae0bbcb769ea113701
sota-f625 <s7716xf625@gmail.com>
2024-07-05 16:58:36 +0900

    フォームオブジェクトモデルの作成

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,20 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :addresses
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
