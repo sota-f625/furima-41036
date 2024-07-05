780ae1a38600e28d3da71ad4c72a6ec512e3f871
sota-f625 <s7716xf625@gmail.com>
2024-07-05 16:51:45 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   root "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  resources :purchases, only: [:index, :create]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :purchases, only: [:index, :create]
+  end
 
 end
