e966cc3dcd07f53a8682647c3e4d68a8ef805fc2
sota-f625 <s7716xf625@gmail.com>
2024-07-08 11:09:02 +0900

    エラーハンドリング表示設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -21,7 +21,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
