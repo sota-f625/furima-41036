17b272740824345eaa932e463992bc54bccdc6e1
sota-f625 <s7716xf625@gmail.com>
2024-07-10 15:13:06 +0900

    コードレビュー後修正_2

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,13 +10,11 @@ class PurchasesController < ApplicationController
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
-    if @purchase_address.valid?
-      pay_item
-      if @purchase_address.save
-        redirect_to root_path
-      else
-        render :index
-      end
+    render :index and return unless @purchase_address.valid?
+
+    pay_item
+    if @purchase_address.save
+      redirect_to root_path
     else
       render :index
     end
@@ -25,7 +23,9 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def set_item
@@ -37,7 +37,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = ENV.fetch('PAYJP_SECRET_KEY', nil)
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
@@ -46,6 +46,6 @@ class PurchasesController < ApplicationController
   end
 
   def set_gon_public_key
-    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
+    gon.public_key = ENV.fetch('PAYJP_PUBLIC_KEY', nil)
   end
 end
